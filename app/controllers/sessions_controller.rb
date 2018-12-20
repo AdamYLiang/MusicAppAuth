18835bb0cd11248b01e1d1e20121f92ffe42c574
AdamYLiang <ayl292@nyu.edu>
2018-12-20 10:48:08 -0500

    Implement Login/Logout/Signup

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,26 @@
 class SessionsController < ApplicationController
 
     def new 
+        @user = User.new
+        render :new
     end
 
     def create
+        email = params[:user][:email]
+        password = params[:user][:password]
+        @user = User.find_by_credentials(email, password)
+        if @user
+            login!(@user)
+            render :new
+        else
+            flash.now[:errors] = ["Invalid email/password"]
+            render :new
+        end
     end
     
     def destroy
+        logout!
+        redirect_to new_session_url
     end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,17 +2,22 @@ class UsersController < ApplicationController
 
     def new
         @user = User.new
+        render :new
     end
 
     def create
         @user = User.new(user_params)
         if @user.save
-            session[:session] = @user.reset_session_token!
+            login!(@user)
             render :new
         else
             flash.now[:errors] = @user.errors.full_messages
             render :new
         end
+    end
+
+    def show
+        @user = User.find(params[:id])
     end
 
     private
